6a6906c122cf78bde8fe56b153b9623f22725909
Andrec1350 <114308210+AndreiCusiac@users.noreply.github.com>
2022-11-17 20:55:39 +0200

    Added getAllBySpecies/Breed in AnimalServiceImpl

--- src/main/java/com/example/tinder/service/implementations/AnimalServiceImpl.java
+++ src/main/java/com/example/tinder/service/implementations/AnimalServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.tinder.service.implementations;
 
 import com.example.tinder.model.entities.Animal;
+import com.example.tinder.model.entities.Breed;
+import com.example.tinder.model.entities.Species;
 import com.example.tinder.repository.AnimalRepository;
 import com.example.tinder.service.interfaces.AnimalService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 public class AnimalServiceImpl implements AnimalService {
@@ -13,5 +16,28 @@ public class AnimalServiceImpl implements AnimalService {
 
     List<Animal> getAll () {
         return animalRepository.findAll();
+    }
+
+    List<Animal> getAllByBreed (Breed breed) {
+
+        Predicate<Animal> isOfBreed = animal -> animal.getBreed() == breed;
+
+        return (List<Animal>) animalRepository.findAll().stream().filter(isOfBreed);
+    }
+
+    List<Animal> getAllBySpecies (Species species) {
+
+        Predicate<Animal> isOfSpecies = animal -> animal.getSpecies() == species;
+
+        return (List<Animal>) animalRepository.findAll().stream().filter(isOfSpecies);
+    }
+
+    List<Animal> getAllByBreedAndSpecies (Breed breed, Species species) {
+        Predicate<Animal> isOfBreed = animal -> animal.getBreed() == breed;
+        Predicate<Animal> isOfSpecies = animal -> animal.getSpecies() == species;
+
+        Predicate<Animal> filterByAll = isOfBreed.and(isOfSpecies);
+
+        return (List<Animal>) animalRepository.findAll().stream().filter(filterByAll);
     }
 }
