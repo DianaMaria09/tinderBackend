57a283b1d335def75d780c8d5a12f4d765b376e2
OanaBuboi <oanabuboi@yahoo.com>
2022-11-17 22:18:05 +0200

    Created endPoint for selected animal for user and all animals for user

--- src/main/java/com/example/tinder/service/interfaces/UserService.java
+++ src/main/java/com/example/tinder/service/interfaces/UserService.java
@@ -1,10 +1,12 @@
 package com.example.tinder.service.interfaces;
 
-import com.example.tinder.model.entities.Chat;
 import com.example.tinder.model.entities.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
     List<User> getAll();
+
+    Optional<User> getById(Long id);
 }
